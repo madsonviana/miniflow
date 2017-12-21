6265a6f9e29cef224c263380382e84f858763ccb
Madson Viana <me@madsonviana.info>
2017-12-21 00:43:13 -0200

    Implementação linear escalar

---
+++ miniflow.py
@@ -0,0 +1,118 @@
+from functools import reduce
+import numpy as np
+
+class Node(object):
+    def __init__(self, inbound_nodes=[]):
+        self.inbound_nodes = inbound_nodes
+        self.outbound_nodes = []
+
+        for n in self.inbound_nodes:
+            n.outbound_nodes.append(self)
+
+        self.value = None
+
+    def forward(self):
+        raise NotImplementedError
+
+class Input(Node):
+    def __init__(self):
+        Node.__init__(self)
+    
+    def forward(self, value=None):
+        if value is not None:
+            self.value = value
+
+class Add(Node):
+    def __init__(self, x, y):
+        Node.__init__(self, [x, y])
+    
+    def forward(self):
+        self.value = reduce(lambda x, y: x + y, map(lambda x: x.value, self.inbound_nodes))
+
+class Mul(Node):
+    def __init__(self, x, y):
+        Node.__init__(self, [x, y])
+    
+    def forward(self):
+        self.value = reduce(lambda x, y: x * y, map(lambda x: x.value, self.inbound_nodes))
+
+class Linear(Node):
+    def __init__(self, inputs, weights, bias):
+        Node.__init__(self, [inputs, weights, bias])
+
+        # NOTE: The weights and bias properties here are not
+        # numbers, but rather references to other nodes.
+        # The weight and bias values are stored within the
+        # respective nodes.
+
+    def forward(self):
+        """
+        Set self.value to the value of the linear function output.
+
+        Your code goes here!
+        """
+        inputs = self.inbound_nodes[0].value
+        weights = self.inbound_nodes[1].value
+        bias = self.inbound_nodes[2].value
+        inputs_dot_weights = map((lambda i, w: i * w), inputs, weights)
+        self.value = reduce((lambda x, y: x + y), inputs_dot_weights) + bias
+
+
+def topological_sort(feed_dict):
+    """
+    Sort generic nodes in topological order using Kahn's Algorithm.
+
+    `feed_dict`: A dictionary where the key is a `Input` node and the value is the respective value feed to that node.
+
+    Returns a list of sorted nodes.
+    """
+
+    input_nodes = [n for n in feed_dict.keys()]
+
+    G = {}
+    nodes = [n for n in input_nodes]
+    while len(nodes) > 0:
+        n = nodes.pop(0)
+        if n not in G:
+            G[n] = {'in': set(), 'out': set()}
+        for m in n.outbound_nodes:
+            if m not in G:
+                G[m] = {'in': set(), 'out': set()}
+            G[n]['out'].add(m)
+            G[m]['in'].add(n)
+            nodes.append(m)
+
+    L = []
+    S = set(input_nodes)
+    while len(S) > 0:
+        n = S.pop()
+
+        if isinstance(n, Input):
+            n.value = feed_dict[n]
+
+        L.append(n)
+        for m in n.outbound_nodes:
+            G[n]['out'].remove(m)
+            G[m]['in'].remove(n)
+            # if no other incoming edges add to S
+            if len(G[m]['in']) == 0:
+                S.add(m)
+    return L
+
+
+def forward_pass(output_node, sorted_nodes):
+    """
+    Performs a forward pass through a list of sorted nodes.
+
+    Arguments:
+
+        `output_node`: A node in the graph, should be the output node (have no outgoing edges).
+        `sorted_nodes`: A topologically sorted list of nodes.
+
+    Returns the output Node's value
+    """
+
+    for n in sorted_nodes:
+        n.forward()
+
+    return output_node.value
